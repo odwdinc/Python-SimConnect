b5cb2ef3e057dd4e597577f763a8c9668b817953
iPhysicX <physicx@netzmuffel.de>
2020-09-01 19:24:22 +0200

    make tests empty

---
+++ tests/test_request.py
@@ -0,0 +1,11 @@
+from SimConnect import Request
+
+from unittest import TestCase
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+class TestSimple(TestCase):
+	def test_init_request(self):
+		self.assertTrue(True)

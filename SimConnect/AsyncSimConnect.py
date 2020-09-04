d9a4706302c3e670c9a2e63b11cae9c4f6e560fd
iPhysicX <physicx@netzmuffel.de>
2020-09-04 23:24:23 +0200

    remove Async at first

--- SimConnect/AsyncSimConnect.py
+++
@@ -1,8 +0,0 @@
-# coming soon
-from .SimConnect import SimConnect, Request
-
-class AsyncSimConnect(SimConnect):
-    async def GetData(self, _Request):
-        pass
-
-    # TODO: add all other functions as async

0bf3d16caa3c60cbd2101e39d0dab9f6ab2144a3
odwdinc <anthony.pray@gmail.com>
2020-09-03 17:31:21 -0700

    boot.py sample for esp32
     [MicroPython](https://micropython.org/) interface with Flask to the esp32

---
+++ esp32_boot_test.py
@@ -0,0 +1,58 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import webrepl
+import network
+import socket
+import json
+
+
+def do_connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('<your ID>', '<your pass>')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+
+def http_get(url):
+    _, _, host, path = url.split('/', 3)
+    addr = socket.getaddrinfo(host, 5000)[0][-1]
+    s = socket.socket()
+    s.connect(addr)
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+    s.close()
+
+
+def http_put(url, body):
+    _, _, host, path = url.split('/', 3)
+    addr = socket.getaddrinfo(host, 5000)[0][-1]
+    s = socket.socket()
+    s.connect(addr)
+    s.send(bytes('PUT /%s HTTP/1.0\r\nHost: %s\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n%s' %
+           (path, host, len(body), body), 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+    s.close()
+
+
+do_connect()
+webrepl.start()
+
+
+http_get('http://<your server ip>/pysim/api/v0.1/requests/1') 
+
+http_put('http://<your server ip>/pysim/api/v0.1/events/4', '{"value": 900}')

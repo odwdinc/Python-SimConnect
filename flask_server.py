c47a95d1448b751eecc7743a20ed01b1f889f8fe
hankhank10 <m.a.hankinson@gmail.com>
2020-09-05 21:27:28 +0100

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -5,21 +5,29 @@ from time import sleep
 app = Flask(__name__)
 
 ### SIMCONNECTION RELATED STARTUPS
+
 # create simconnection
 sm = SimConnect()
+
 # create Request
 myRequest = sm.newRequest(time=2000)  # set auto data collection time @ 2s
+
 # add required definitions output data name, definition from SDK
 myRequest.add('Altitude', (b'Plane Altitude', b'feet'))
 myRequest.add('Latitude', (b'Plane Latitude', b'degrees'))
 myRequest.add('Longitude', (b'Plane Longitude', b'degrees'))
 myRequest.add('Kohlsman', (b'Kohlsman setting hg', b'inHg'))
 
+
+@app.route('/json/')
 @app.route('/json/all')
 def json_add_data():
-    sm.RequestData(myRequest)
-    sm.Run()
-    data = sm.GetData(myRequest)
+
+    data = None
+    while data is None:
+        sm.RequestData(myRequest)
+        sm.Run()
+        data = sm.GetData(myRequest)
 
     data_dictionary = {
         "Altitude": data.Altitude,

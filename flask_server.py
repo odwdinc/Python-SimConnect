bdb0c61d303aac7f696af35eeb6e250a9484746b
hankhank10 <m.a.hankinson@gmail.com>
2020-09-05 21:18:15 +0100

    Tidy

--- flask_server.py
+++ flask_server.py
@@ -1,32 +1,31 @@
+from Flask import flask
 from SimConnect import *
 from time import sleep
 
+app = Flask(__name__)
+
+### SIMCONNECTION RELATED STARTUPS
 # create simconnection
 sm = SimConnect()
-
 # create Request
 myRequest = sm.newRequest(time=2000)  # set auto data collection time @ 2s
-
 # add required definitions output data name, definition from SDK
 myRequest.add('Altitude', (b'Plane Altitude', b'feet'))
 myRequest.add('Latitude', (b'Plane Latitude', b'degrees'))
 myRequest.add('Longitude', (b'Plane Longitude', b'degrees'))
 myRequest.add('Kohlsman', (b'Kohlsman setting hg', b'inHg'))
 
-while 1:
-	sm.RequestData(myRequest)
-	sm.Run()
-	data = sm.GetData(myRequest)
+@app.route('/json/all')
+def json_add_data():
+    sm.RequestData(myRequest)
+    sm.Run()
+    data = sm.GetData(myRequest)
 
-	if data is not None:
-		print("Lat=%f  Lon=%f  Alt=%f Kohlsman=%.2f" % (
-			data.Latitude,
-			data.Longitude,
-			data.Altitude,
-			data.Kohlsman
-		))
-		print (data)
-		
-	sleep (1)
 
-sm.Exit()
+
+
+
+
+
+
+app.run(host='0.0.0.0', port=5000, debug=True)

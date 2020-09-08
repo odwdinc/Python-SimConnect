b9cbdc5ae820be0912edb351b1fac669dfebad8f
odwdinc <anthony.pray@gmail.com>
2020-09-08 15:16:56 -0700

    v bump

--- SimConnect/__init__.py
+++ SimConnect/__init__.py
@@ -10,7 +10,7 @@ def int_or_str(value):
 		return value
 
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 VERSION = tuple(map(int_or_str, __version__.split(".")))
 
 __all__ = ["SimConnect", "Request", "Event", "millis", "DWORD", "Plane", "AircraftRequests", "AircraftEvents"]

9f32a54a1d61af8a5077352644f73a9a1f46c098
hankhank10 <m.a.hankinson@gmail.com>
2020-09-11 08:20:53 +0100

    New comment

--- glass_server.py
+++ glass_server.py
@@ -368,6 +368,8 @@ def get_datapoint(datapoint_name, index=None):
 
 @app.route('/datapoint/<datapoint_name>/get', methods=["GET"])
 def get_datapoint_endpoint(datapoint_name):
+	# This is the http endpoint wrapper for getting a datapoint
+
 	ds = request.get_json() if request.is_json else request.form
 	index = ds.get('index')
 

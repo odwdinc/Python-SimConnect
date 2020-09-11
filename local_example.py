9ed25e70e2f05180adb918daa77e8fdd34557a4a
odwdinc <anthony.pray@gmail.com>
2020-09-11 09:47:33 -0700

    v Bump

--- local_example.py
+++ local_example.py
@@ -12,23 +12,9 @@ ct_g = millis()
 
 # creat simconnection and pass used user classes
 sm = SimConnect()
-
-
-ae = AircraftEvents(sm)
-# PARKING_BRAKES = Event(b'PARKING_BRAKES', sm)
-# long path
-PARKING_BRAKES = ae.Miscellaneous_Systems.PARKING_BRAKES
-# using get
-GEAR_TOGGLE = ae.Miscellaneous_Systems.get("GEAR_TOGGLE")
-# Using find to lookup Event
-# Note at this time find is limmed to AircraftEvents
-AP_MASTER = ae.find("AP_MASTER")
-
-# THROTTLE1 Event
-THROTTLE1 = ae.Engine.THROTTLE1_SET
-
-
 aq = AircraftRequests(sm)
+ae = AircraftEvents(sm)
+
 
 # Set pos arund space nedle in WA.
 sm.set_pos(
@@ -42,12 +28,26 @@ sm.set_pos(
 	# _OnGround=0
 )
 
+# PARKING_BRAKES = Event(b'PARKING_BRAKES', sm)
+# long path
+PARKING_BRAKES = ae.Miscellaneous_Systems.PARKING_BRAKES
+# using get
+GEAR_TOGGLE = ae.Miscellaneous_Systems.get("GEAR_TOGGLE")
+# Using find to lookup Event
+AP_MASTER = ae.find("AP_MASTER")
+
+# THROTTLE1 Event
+THROTTLE1 = ae.Engine.THROTTLE1_SET
+
 
 # THROTTLE1 Request
-Throttle = aq.find('GENERAL_ENG_THROTTLE_LEVER_POSITION:index')
+Throttle = aq.find('GENERAL_ENG_THROTTLE_LEVER_POSITION:1')
+
+# If useing
+# Throttle = aq.find('GENERAL_ENG_THROTTLE_LEVER_POSITION:index')
 # Need to set index befor read/write
 # Note to set index 2 vs 1 just re-run
-Throttle.setIndex(1)
+# Throttle.setIndex(1)
 
 
 # print the built in description
@@ -74,6 +74,8 @@ while not sm.quit:
 
 	# Send Event toggle AP_MASTER
 	# AP_MASTER()
+
+	# PARKING_BRAKES()
 
 	# send new data inine @ 5s
 	if ct_g + 5000 < millis():

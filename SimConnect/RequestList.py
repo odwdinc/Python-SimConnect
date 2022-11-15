e07ec2798bf40b25108b15ff8bf1a26d30656333
Pomax <pomax@nihongoresources.com>
2022-11-15 10:36:26 -0800

    Add support for Camera variables
    Added all variables listed on https://docs.flightsimulator.com/html/Programming_Tools/SimVars/Camera_Variables.htm

--- SimConnect/RequestList.py
+++ SimConnect/RequestList.py
@@ -228,6 +228,8 @@ class AircraftRequests():
 		self.list.append(self.Racing)
 		self.EnvironmentData = self.__EnvironmentData(_sm, _time, _attemps)
 		self.list.append(self.EnvironmentData)
+		self.CameraData = self.__Camera(_sm, _time, _attemps)
+		self.list.append(self.CameraData)
 		self.SlingsandHoists = self.__SlingsandHoists(_sm, _time, _attemps)
 		self.list.append(self.SlingsandHoists)
 
@@ -1169,4 +1171,16 @@ class AircraftRequests():
 			"LOCAL_DAY_OF_YEAR": ["Local day of year", b'LOCAL DAY OF YEAR', b'Number', 'N'],
 			"LOCAL_YEAR": ["Local year", b'LOCAL YEAR', b'Number', 'N'],
 			"TIME_ZONE_OFFSET": ["Local time difference from GMT", b'TIME ZONE OFFSET', b'Seconds', 'N'],
+		}
+
+	class __Camera(RequestHelper):
+		list = {
+			"CAMERA_GAMEPLAY_PITCH_YAW:index": ["Returns either the pitch (index 0) or the yaw (index 1) of the current gameplay camera.", b'CAMERA GAMEPLAY PITCH YAW:index', b'Radians', 'N'],
+			"CAMERA_REQUEST_ACTION": ["This can be used to have the currently active camera perform a predefined action.", b'CAMERA REQUEST ACTION', b'Enum', 'Y'],
+			"CAMERA_STATE": ["This can be used to get or set the camera 'state', which will be one of the listed enum values.", b'CAMERA STATE', b'Enum', 'Y'],
+			"CAMERA_SUBSTATE": ["This variable can be used to get or set the camera 'sub-state'.", b'CAMERA SUBSTATE', b'Enum', 'Y'],
+			"CAMERA_VIEW_TYPE_AND_INDEX:index": ["Get or set both the type of view for the current camera, as well as the option index.", b'CAMERA VIEW TYPE AND INDEX:index', b'Enum', 'Y'],
+			"CAMERA_VIEW_TYPE_AND_INDEX_MAX:index": ["Get the number of option indices related to a specific camera view type.", b'CAMERA VIEW TYPE AND INDEX MAX:index', b'Number', 'Y'],
+			"GAMEPLAY_CAMERA_FOCUS": ["This gets/sets the focus for the camera zoom.", b'GAMEPLAY CAMERA FOCUS', b'Number', 'Y'],
+			"IS_CAMERA_RAY_INTERSECT_WITH_NODE": ["Check for a collision along a ray from the center of the user FOV and a model node.", b'IS CAMERA RAY INTERSECT WITH NODE', b'Bool', 'N'],
 		}
